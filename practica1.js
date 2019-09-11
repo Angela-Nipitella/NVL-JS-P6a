e0ccca28167745cad22ee58c30556b51d1324a13
Angela Nipitella <nipitellaangela@gmail.com>
2019-09-12 00:05:42 +0200

    practica 1

---
+++ practica1.js
@@ -0,0 +1,12 @@
+// primera practica
+/* Tu nombre y apellidos
+Tu edad
+Los días de la semana
+El valor de la gravedad
+Los 3 primeros números impares*/
+
+var miNombre = "Angela Nipitella" ;
+var miEdad = 28;
+var diasSemana = "Lunes, Martes, Miercoles, Jueves, Viernes, Sabado y Domingo"
+var valorGravedad = "9.8 m/s2"
+var primerosNumerosImp = [1, 3, 5]
